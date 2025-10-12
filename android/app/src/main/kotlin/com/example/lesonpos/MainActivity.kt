c2b74fef279efe5ce0255c135b3c860d43d4a3dd
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-12 03:26:20 +0100

    Split Android APK build to guarantee arm64 artifact

---
+++ android/app/src/main/kotlin/com/example/lesonpos/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lesonpos
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        EpsonDirectPrinterPlugin.registerWith(
+            flutterEngine.dartExecutor.binaryMessenger,
+            applicationContext
+        )
+    }
+}
